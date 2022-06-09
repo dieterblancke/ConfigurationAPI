5fea959678b5928175bf1cf42d55134e5e8b0320
Dieter Blancke <dieter.blancke@outlook.com>
2022-06-09 10:42:46 +0200

    attempted fix

--- src/main/java/be/dieterblancke/configuration/yaml/YamlConfiguration.java
+++ src/main/java/be/dieterblancke/configuration/yaml/YamlConfiguration.java
@@ -173,7 +173,9 @@ public class YamlConfiguration extends YamlSection implements IConfiguration
     @SneakyThrows
     private String dumpWithComments()
     {
-        return new YamlCommentDumper( this.parseComments(), new StringReader( this.dumpWithoutComments() ) ).dump();
+        String yamlWithoutComments = this.dumpWithoutComments();
+
+        return new YamlCommentDumper( this.parseComments( yamlWithoutComments ), new StringReader( yamlWithoutComments ) ).dump();
     }
 
     private String dumpWithoutComments()
@@ -202,15 +204,6 @@ public class YamlConfiguration extends YamlSection implements IConfiguration
     public String getComment( final String path, final CommentType type )
     {
         return this.options.isUseComments() && this.yamlCommentMapper != null ? this.yamlCommentMapper.getComment( path, type ) : null;
-    }
-
-    private YamlCommentMapper parseComments()
-    {
-        if ( this.yamlCommentMapper != null )
-        {
-            return this.yamlCommentMapper;
-        }
-        return parseComments( fileToString() );
     }
 
     @SneakyThrows
